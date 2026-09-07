2ff5c47552356f436d65b2241803f21230187a64
Shivam <Shivamkumarrawani51@gmail.com>
2026-09-07 15:04:02 +0530

    fix cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,26 +13,10 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// CORS
 app.use(cors({
-    origin: function (origin, callback) {
-        const allowedOrigins = [
-            'http://localhost:5173',
-            'https://zomato-clone-1-uyv5.onrender.com'
-        ];
-
-        // Allow requests without an origin
-        // (Postman, server-to-server requests, etc.)
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: true,
     credentials: true
 }));
-
-// Test route
 app.get('/', (req, res) => {
     res.send('hello world');
 });
